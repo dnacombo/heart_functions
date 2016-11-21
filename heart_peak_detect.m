f1de19d55c40d36c7731f7935927d626a904b80e
dnacombo <maximilien.chaumon@gmail.com>
2016-11-21 17:24:10 +0100

    fix nanmean template

--- heart_peak_detect.m
+++ heart_peak_detect.m
@@ -183,7 +183,7 @@ for ii=1:length(beats)
     end
 end
 
-mHB = mean(HB,1);
+mHB = nanmean(HB,1);
 
 % we'll assume that signal at dt seconds before detected R-peaks should be
 % lower than R-peaks. 50 ms seems reasonable.
@@ -387,7 +387,7 @@ end
 
 
 
-function [p, v] = peakdetect2(dat, val, mindist)
+function [p, v] = toto(dat, val, mindist)
 
 % PEAKDETECT2 detects peaks above a certain threshold in single-channel data
 %
@@ -471,5 +471,55 @@ x = x(inan);
 [ztmp,mu,sigma] = zscore(x,flag,dim);
 z(inan) = ztmp;
 
+
+
+function [locs, pks]=peakdetect2(x,minpeakh,minpeakdist)
+% [locs, pks]=peakseek(x,minpeakdist,minpeakh)
+% Alternative to the findpeaks function.  This thing runs much much faster.
+% It really leaves findpeaks in the dust.  It also can handle ties between
+% peaks.  Findpeaks just erases both in a tie.  Shame on findpeaks.
+%
+% x is a vector input (generally a timecourse)
+% minpeakdist is the minimum desired distance between peaks (optional, defaults to 1)
+% minpeakh is the minimum height of a peak (optional)
+%
+% (c) 2010
+% Peter O'Connor
+% peter<dot>ed<dot>oconnor .AT. gmail<dot>com
+
+if size(x,2)==1, x=x'; end
+
+% Find all maxima and ties
+locs=find(x(2:end-1)>=x(1:end-2) & x(2:end-1)>=x(3:end))+1;
+
+if nargin<2, minpeakdist=1; end % If no minpeakdist specified, default to 1.
+
+if nargin>2 % If there's a minpeakheight
+    locs(x(locs)<=minpeakh)=[];
+end
+
+if minpeakdist>1
+    while 1
+
+        del=diff(locs)<minpeakdist;
+
+        if ~any(del), break; end
+
+        pks=x(locs);
+
+        [garb mins]=min([pks(del) ; pks([false del])]); %#ok<ASGLU>
+
+        deln=find(del);
+
+        deln=[deln(mins==1) deln(mins==2)+1];
+
+        locs(deln)=[];
+
+    end
+end
+
+if nargout>1,
+    pks=x(locs);
+end
 
 
