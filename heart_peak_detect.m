e878a108f20872f48a7878d4c6419bd09691e086
Maximilien Chaumon <maximilien.chaumon@icm-institute.org>
2019-08-19 12:25:13 +0200

    add GPL license

--- heart_peak_detect.m
+++ heart_peak_detect.m
@@ -88,6 +88,23 @@ function [HeartBeats, R_time] = heart_peak_detect(cfg,data)
 %   standard...).
 %  
 
+% heart_functions is a program meant to detect heart beats in an electrocardiogram and compute stroke volume.
+%
+%    Copyright (C) 2019  Maximilien Chaumon
+%
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+%
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+%
+%    You should have received a copy of the GNU General Public License
+%    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 % v0 Maximilien Chaumon November 2016
 % based on previously undocumented anonymous version
 
